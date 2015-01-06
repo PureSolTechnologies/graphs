3678a8637dce8dad34e0f891d7792feb910b1d35
Rick-Rainer Ludwig <ludwig@puresol-technologies.com>
2015-01-06 17:32:17 +0100

    * Removed ObjectUtilities due to replacement from JDK: Objects.hash(...) * New package structure in misc. * Added protected void setState(...) in AbstractStateModel.

--- src/main/java/com/puresoltechnologies/commons/statemodel/AbstractStateModel.java
+++ src/main/java/com/puresoltechnologies/commons/statemodel/AbstractStateModel.java
@@ -7,48 +7,52 @@ package com.puresoltechnologies.commons.statemodel;
  * 
  */
 public abstract class AbstractStateModel<S extends State<S, T>, T extends Transition<S, T>>
-	implements StateModel<S, T> {
+		implements StateModel<S, T> {
 
-    private S currentState;
+	private S currentState;
 
-    /**
-     * This is the default constructor. It starts the model at the start state
-     * as returned in {@link #getStartState()}.
-     */
-    public AbstractStateModel() {
-	super();
-	currentState = getStartState();
-    }
-
-    /**
-     * This constructor starts at a given initial state. This can be useful in
-     * cases were a model was persisted to restart it.
-     * 
-     * @param initialState
-     */
-    public AbstractStateModel(S initialState) {
-	super();
-	currentState = initialState;
-    }
-
-    @Override
-    public final S getState() {
-	return currentState;
-    }
-
-    @Override
-    public boolean canPerformTransition(T transition) {
-	return currentState.getTransitions().contains(transition);
-    }
-
-    @Override
-    public final void performTransition(T transition) {
-	if (!canPerformTransition(transition)) {
-	    throw new IllegalStateException("Transition '"
-		    + transition.getName() + "' is not allowed in state '"
-		    + currentState.getName() + "'");
+	/**
+	 * This is the default constructor. It starts the model at the start state
+	 * as returned in {@link #getStartState()}.
+	 */
+	public AbstractStateModel() {
+		super();
+		currentState = getStartState();
 	}
-	currentState = transition.getTargetState();
-    }
+
+	/**
+	 * This constructor starts at a given initial state. This can be useful in
+	 * cases were a model was persisted to restart it.
+	 * 
+	 * @param initialState
+	 */
+	public AbstractStateModel(S initialState) {
+		super();
+		currentState = initialState;
+	}
+
+	@Override
+	public final S getState() {
+		return currentState;
+	}
+
+	protected final void setState(S state) {
+		currentState = state;
+	}
+
+	@Override
+	public boolean canPerformTransition(T transition) {
+		return currentState.getTransitions().contains(transition);
+	}
+
+	@Override
+	public final void performTransition(T transition) {
+		if (!canPerformTransition(transition)) {
+			throw new IllegalStateException("Transition '"
+					+ transition.getName() + "' is not allowed in state '"
+					+ currentState.getName() + "'");
+		}
+		currentState = transition.getTargetState();
+	}
 
 }
